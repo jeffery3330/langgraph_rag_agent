d6efda08a2fa512e2b6254bdfac2378cfcc274fd
b10902040 <b10902040@ntu.edu.tw>
2024-08-13 21:48:37 +0800

    dependencies fixed

--- app.py
+++ app.py
@@ -198,5 +198,5 @@ retriever = get_retriever(filenames = filenames)
 print("pdfs loaded.")
     
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host='0.0.0.0', port=5000)
     # app.run(host='0.0.0.0', port=5000)
